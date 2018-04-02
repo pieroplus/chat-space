604bf6ee61ff29ad88dfe3e367ec4470a232f9d5
pieroplus <gussunoyoyo@hotmail.co.jp>
2018-04-02 17:32:22 +0900

    create-ajax

---
+++ app/assets/javascripts/chat-space.js
@@ -0,0 +1,48 @@
+$(function(){
+  function buildHTML(message){
+    if (message.image != null){
+      var image = `${message.image}`
+    } else {
+      var image = `""`
+    }
+    var html = `<p class="chates__list__user">
+                  ${message.user}
+                </p>
+                <p class="chates__list__time">
+                  ${message.date}
+                </p>
+                <p class="chates__list__message">
+                  ${message.body}
+                </p>
+                <p class="chates__list__image">
+                <img src ="${image}">
+                </p>`
+    return html;
+  }
+  $('.post-zone').on('submit',function(e){
+    e.preventDefault();
+    var formData = new FormData(this)
+    var href = window.location.href
+    $.ajax({
+      url: href,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      timeout: 10000,
+      processData: false,
+      contentType: false,
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chates__list').append(html)
+      $('.message-area').val('')
+      $('.display-none').val('')
+      $('.chat-space').animate({scrollTop: $('.chat-space')[0].scrollHeight}, 'fast');
+      $(".send-btn").prop("disabled", false);
+    })
+    .fail(function(data){
+      alert("error");
+      $(".send-btn").prop("disabled", false);
+    });
+  });
+});
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,4 @@
+json.user @message.user.name
+json.body @message.body
+json.date @message.created_at
+json.image @message.image_url
